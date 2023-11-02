cbbefe8a16473f58dd9dda8b9f5101c7a8fe3e8e
Tomas Bance <tomas.bance@gmail.com>
2023-11-02 13:24:34 +0100

    testing is it runs

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.get('/api', (req, res) => {
+    res.json({success: true})
+})
 
 //enable cors;
 app.use(cors())
