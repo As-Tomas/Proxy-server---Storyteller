496487c0b649c5d1e516b15a641f66c15bfa11b9
Tomas Bance <tomas.bance@gmail.com>
2023-11-02 16:24:38 +0100

    express-rate-limit for api calls

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60* 1000, //10 minutes
+    max: 5 //limit each IP to 1 requests per windowMs
+}); 
+
+app.use(limiter);
+app.set('trust proxy', 1);
 
 //enable cors;
 app.use(cors())
