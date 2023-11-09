ba6ee27e3ffd4eb8764e5934b5f2e7a4498b5b71
Tomas Bance <tomas.bance@gmail.com>
2023-11-09 13:52:29 +0100

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+// const express = require('express');
+// const cors = require('cors');
+// const rateLimit = require('express-rate-limit');
+
+// require('dotenv').config();
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// // Parse JSON bodies (as sent by API clients)
+// app.use(express.json());
+
+
+// //rate limiting
+// const limiter = rateLimit({
+//     windowMs: 10 * 60* 1000, //10 minutes
+//     max: 10 //limit each IP to 1 requests per windowMs
+// }); 
+
+
+// // Middleware to check for API key in the request header
+// function checkApiKey(req, res, next) {
+//     const apiKey = req.get('X-API-Key'); // Replace 'X-API-Key' with the name of your header
+//     if (!apiKey || apiKey !== process.env.SRV_USR_API) {
+//       return res.status(401).json({ success: false, message: 'Invalid or missing API key' });
+//     }
+//     next();
+//   }
+
+// app.use(limiter);
+// app.set('trust proxy', 1);
+
+// //enable cors;
+// app.use(cors())
+
+// // Routes
+// app.use('/api', checkApiKey, require('./routes/midjourneyApi'))
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
@@ -11,31 +54,52 @@ const app = express();
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-
-//rate limiting
+// Rate limiting
 const limiter = rateLimit({
-    windowMs: 10 * 60* 1000, //10 minutes
-    max: 10 //limit each IP to 1 requests per windowMs
-}); 
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // Limit each IP to 100 requests per windowMs
+});
 
+app.use(limiter);
+app.set('trust proxy', 1);
+
+// Enable CORS
+app.use(cors());
 
 // Middleware to check for API key in the request header
 function checkApiKey(req, res, next) {
     const apiKey = req.get('X-API-Key'); // Replace 'X-API-Key' with the name of your header
     if (!apiKey || apiKey !== process.env.SRV_USR_API) {
-      return res.status(401).json({ success: false, message: 'Invalid or missing API key' });
+        return res.status(401).json({ success: false, message: 'Invalid or missing API key' });
     }
     next();
-  }
-
-app.use(limiter);
-app.set('trust proxy', 1);
-
-//enable cors;
-app.use(cors())
+}
 
 // Routes
-app.use('/api', checkApiKey, require('./routes/midjourneyApi'))
+app.use('/api', checkApiKey, require('./routes/midjourneyApi'));
+
+// Endpoint for Server-Sent Events
+app.get('/events', (req, res) => {
+    // Set headers for SSE
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    });
+
+    // Send a welcome message on connection
+    res.write(`data: ${JSON.stringify({ message: 'Connected to update stream' })}\n\n`);
+
+    // Keep the connection open by sending a comment
+    const keepAliveInterval = setInterval(() => {
+        res.write(': keep-alive\n\n');
+    }, 20000);
+
+    // Remove the keep-alive interval when the connection is closed
+    req.on('close', () => {
+        clearInterval(keepAliveInterval);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- routes/midjourneyApi.js
+++ routes/midjourneyApi.js
@@ -1,81 +1,227 @@
+// const express = require("express");
+// const router = express.Router();
+// const url = require("url");
+// const needle = require("needle");
+// const { Midjourney } = require("midjourney");
+
+// // This object will hold the SSE connections
+// const clients = {};
+
+// // Establish an SSE connection
+// router.get('/events', (req, res) => {
+//   const clientId = Date.now(); // Generate a unique client ID
+//   const headers = {
+//     'Content-Type': 'text/event-stream',
+//     'Connection': 'keep-alive',
+//     'Cache-Control': 'no-cache'
+//   };
+//   res.writeHead(200, headers);
+
+//   // Construct a simple SSE response message
+//   const data = `data: ${JSON.stringify({ message: "Connection established" })}\n\n`;
+//   res.write(data);
+
+//   // Store this client's response object so we can send updates later
+//   clients[clientId] = res;
+
+//   // When the client closes the connection, remove the corresponding response object
+//   req.on('close', () => {
+//     console.log(`Client ${clientId} connection closed`);
+//     delete clients[clientId];
+//   });
+// });
+
+// // Function to send updates to all clients
+// function sendUpdateToClients(update) {
+//   Object.keys(clients).forEach(clientId => {
+//     clients[clientId].write(`data: ${JSON.stringify(update)}\n\n`);
+//   });
+// }
+
+// const client = new Midjourney({
+//   ServerId: process.env.SERVER_ID,
+//   ChannelId: process.env.CHANNEL_ID,
+//   SalaiToken: process.env.SALAI_TOKEN,
+//   Debug: false,
+//   Ws: true, //enable ws is required for remix mode (and custom zoom)
+// });
+
+// router.get("/imagine", async (req, res) => {
+//   const requestData = req.body;
+
+//   if (process.env.NODE_ENV !== "production") {
+//     console.log("Request query ", url.parse(req.url, true).query);
+//     console.log("Request body: ", requestData);
+//     //console.log("Request headers: ", req.headers);
+//   }
+
+//   await client.init();
+
+//   // Instead of sending the response immediately, send a job accepted message
+//   const jobId = Date.now(); // Generate a unique job ID
+//   sendUpdateToClients({ jobId, status: 'accepted', message: 'Job accepted and queued for processing.' });
+
+
+//   try {
+//     const prompt = requestData.prompt;
+//     if (prompt === undefined) {
+//       console.log("no prompt");
+//       res.status(400).json({ success: false, error: "no prompt" });
+//     }
+
+//     //imagine
+//     const Imagine = await client.Imagine(prompt, (uri, progress) => {
+//       console.log("loading", uri, "progress", progress);
+//     });
+
+//     console.log(Imagine);
+
+//     if (!Imagine) {
+//       console.log("no message");
+//       res.status(400).json({ success: false });
+//     } else {
+//       const U1CustomID = Imagine.options?.find((o) => o.label === "U1")?.custom;
+//       if (!U1CustomID) {
+//         console.log("no U1");
+//         res.status(400).json({ success: false, error: "no U1" });
+//       }
+      
+//       // Upscale U1
+      
+//       const Upscale = await client.Custom({
+//         msgId: Imagine.id,
+//         flags: Imagine.flags,
+//         customId: U1CustomID,
+//         content: prompt,
+//         loading: (uri, progress) => {
+//           console.log("loading", uri, "progress", progress);
+//         },
+//       });    
+
+//       console.log(Upscale);
+
+//       if (!Upscale) {
+//         console.log("no Upscale");
+//         res.status(400).json({ success: false, error: "no Upscale" });
+//       }
+
+//       console.log("Upscale ", Upscale);
+
+//       console.log("link to all img " + Imagine.uri);
+//       console.log("link to upscaled img " + Upscale.uri);
+//       //res.status(200).json({ success: true, img: Upscale.uri });
+
+//       // Instead of sending the response immediately, send a job accepted message
+//   const jobId = Date.now(); // Generate a unique job ID
+//   sendUpdateToClients({ jobId, status: 'accepted', message: 'Job accepted and queued for processing.' });
+
+//     }
+//   } catch (error) {
+//     console.error("Error: ", error.message);
+//     console.error("Stack Trace: ", error.stack);
+//     //res.status(500).json({ success: false, error: error.message });
+//     sendUpdateToClients({ jobId, status: 'error', message: error.message });
+//   }
+// });
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
-const url = require("url");
-const needle = require("needle");
 const { Midjourney } = require("midjourney");
 
-const client = new Midjourney({
-  ServerId: process.env.SERVER_ID,
-  ChannelId: process.env.CHANNEL_ID,
-  SalaiToken: process.env.SALAI_TOKEN,
-  Debug: false,
-  Ws: true, //enable ws is required for remix mode (and custom zoom)
+// This will store client connections for SSE
+const clients = {};
+
+// Helper function to send data to all clients
+function sendToAllClients(data) {
+  Object.keys(clients).forEach(clientID => {
+    clients[clientID].write(`data: ${JSON.stringify(data)}\n\n`);
+  });
+}
+
+// SSE endpoint to establish a connection and send updates
+router.get('/events', (req, res) => {
+  // Generate a unique client ID
+  const clientId = Date.now();
+
+  // Set headers for SSE
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  });
+
+  // Add this client to the clients object
+  clients[clientId] = res;
+
+  // Send a welcome message on connection
+  res.write(`data: ${JSON.stringify({ message: 'Connected to update stream' })}\n\n`);
+
+  // Remove this client when the connection is closed
+  req.on('close', () => {
+    delete clients[clientId];
+  });
 });
 
+// Your existing /imagine endpoint modified to use SSE
 router.get("/imagine", async (req, res) => {
+  // Extract the prompt from the request
   const requestData = req.body;
-
-  if (process.env.NODE_ENV !== "production") {
-    console.log("Request query ", url.parse(req.url, true).query);
-    console.log("Request body: ", requestData);
-    //console.log("Request headers: ", req.headers);
+  const prompt = requestData.prompt;
+  if (!prompt) {
+    return res.status(400).json({ success: false, error: "No prompt provided" });
   }
 
-  await client.init();
+  // Initialize the client and send a job accepted message
+  const jobId = Date.now();
+  sendToAllClients({ jobId, status: 'accepted', message: 'Job accepted and processing started.' });
 
   try {
-    const prompt = requestData.prompt;
-    if (prompt === undefined) {
-      console.log("no prompt");
-      res.status(400).json({ success: false, error: "no prompt" });
-    }
+    await client.init();
 
-    //imagine
+    // Imagine process
     const Imagine = await client.Imagine(prompt, (uri, progress) => {
-      console.log("loading", uri, "progress", progress);
+      // Send progress updates to the client
+      sendToAllClients({ jobId, status: 'in-progress', progress, uri });
     });
 
-    console.log(Imagine);
-
     if (!Imagine) {
-      console.log("no message");
-      res.status(400).json({ success: false });
-    } else {
-      const U1CustomID = Imagine.options?.find((o) => o.label === "U1")?.custom;
-      if (!U1CustomID) {
-        console.log("no U1");
-        res.status(400).json({ success: false, error: "no U1" });
-      }
-      
-      // Upscale U1
-      
-      const Upscale = await client.Custom({
-        msgId: Imagine.id,
-        flags: Imagine.flags,
-        customId: U1CustomID,
-        content: prompt,
-        loading: (uri, progress) => {
-          console.log("loading", uri, "progress", progress);
-        },
-      });    
-
-      console.log(Upscale);
-
-      if (!Upscale) {
-        console.log("no Upscale");
-        res.status(400).json({ success: false, error: "no Upscale" });
-      }
-
-      console.log("Upscale ", Upscale);
-
-      console.log("link to all img " + Imagine.uri);
-      console.log("link to upscaled img " + Upscale.uri);
-      res.status(200).json({ success: true, img: Upscale.uri });
+      throw new Error('No response from Imagine API');
     }
+
+    const U1CustomID = Imagine.options?.find((o) => o.label === "U1")?.custom;
+    if (!U1CustomID) {
+      throw new Error('No U1 option found');
+    }
+
+    // Upscale process
+    const Upscale = await client.Custom({
+      msgId: Imagine.id,
+      flags: Imagine.flags,
+      customId: U1CustomID,
+      content: prompt,
+      loading: (uri, progress) => {
+        // Send progress updates to the client
+        sendToAllClients({ jobId, status: 'in-progress', progress, uri });
+      },
+    });
+
+    if (!Upscale) {
+      throw new Error('No response from Upscale API');
+    }
+
+    // When the image is ready, send an update to the client
+    sendToAllClients({ jobId, status: 'completed', img: Upscale.uri });
+
+    // Respond to the initial HTTP request to acknowledge it was received
+    res.json({ success: true, message: 'Job accepted, updates will be sent via SSE.' });
   } catch (error) {
-    console.error("Error: ", error.message);
-    console.error("Stack Trace: ", error.stack);
-    res.status(500).json({ success: false, error: error.message });
+    // If an error occurs, send an error update to the client
+    sendToAllClients({ jobId, status: 'error', message: error.message });
+
+    // Respond to the initial HTTP request with the error
+    res.status(500).json({ success: false, message: 'An error occurred, please check the SSE for details.' });
   }
 });
 
