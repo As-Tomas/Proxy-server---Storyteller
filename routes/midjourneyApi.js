80b0d4fbfa986332b5fb172b0f9f2ac3ad155e58
Tomas Bance <tomas.bance@gmail.com>
2023-11-21 12:51:56 +0100

    Refactoring, split SSE to sep comp

--- routes/midjourneyApi.js
+++ routes/midjourneyApi.js
@@ -1,67 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { Midjourney } = require("midjourney");
+const { sse, sendToClient } = require('./sse');
 
-// This will store client connections for SSE
-const clients = {};
-
-// Helper function to send data to a specific client
-function sendToClient(clientId, data) {
-  const client = clients[clientId];
-  console.log(`Attempting to send message to client ${clientId}`);
-  if (client && !client.finished) { // Check if the client exists and the response is not finished
-    console.log(`Sending message to client ${clientId}`);
-    client.write(`data: ${JSON.stringify(data)}\n\n`);
-  } else {
-    console.log(`Client ${clientId} not found or response finished`);
-  }
-}
-
-// SSE endpoint to establish a connection and send updates
-router.get("/events", (req, res) => {
-  const requestHeaders = req.headers;
-  const userName = requestHeaders['username'];
-  // Generate a unique client ID
-  const clientId = userName ;
-  console.log(`Client connected with ID: ${clientId}`);
-
-  res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    "Connection": "keep-alive",
-    "Cache-Control": "no-cache",
-    "Access-Control-Allow-Origin": "*", // Allow any domain to connect
-  });
-
-  clients[clientId] = res;
-  console.log(`Added client. Total clients: ${Object.keys(clients).length}`);
-
-  // Send a welcome message on connection
-  const welcomeData = { message: "Connected to update stream" };
-  sendToClient(clientId, welcomeData);
-  console.log(`Sent welcome message to client ${clientId}`);
-
-  // Keep the connection open by sending a comment
-  const keepAliveInterval = setInterval(() => {
-    if (res.writableEnded) { // Check if the client has disconnected
-      clearInterval(keepAliveInterval); // Stop the keep-alive messages
-      delete clients[clientId]; // Clean up the clients object
-      console.log(`Stopped keep-alive for client ID: ${clientId}`);
-    } else {
-      res.write(': keep-alive\n\n');
-    }
-  }, 20000); // Send a keep-alive every 20 seconds
-
-  // Log when the connection is closed
-  req.on("close", () => {
-    clearInterval(keepAliveInterval);
-    console.log(`Client disconnected with ID: ${clientId}`);
-    delete clients[clientId];
-    console.log(`Removed client. Total clients: ${Object.keys(clients).length}`);
-  });
-
-  const data = {message: 'Job done', result: 'Your result here' };
-  sendToClient(clientId, data);
-});
+router.get("/events", sse);
 
 // Your existing /imagine endpoint modified to use SSE
 router.post("/imagine", async (req, res) => {
